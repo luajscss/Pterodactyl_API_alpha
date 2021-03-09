d639564e0b3fcec77660232b5816ab0bdecf5afa
luas <70727706+luajscss@users.noreply.github.com>
2021-03-09 02:55:40 +0200

    release

---
+++ src/hittsad/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿
+namespace hittsad
+{
+    partial class LAPI
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(LAPI));
+            this.Start = new System.Windows.Forms.Button();
+            this.Restart = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.VK = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.Color.Lime;
+            this.Start.ForeColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.Start, "Start");
+            this.Start.Name = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Restart
+            // 
+            this.Restart.BackColor = System.Drawing.Color.Yellow;
+            this.Restart.ForeColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.Restart, "Restart");
+            this.Restart.Name = "Restart";
+            this.Restart.UseVisualStyleBackColor = false;
+            this.Restart.Click += new System.EventHandler(this.Restart_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Orange;
+            this.button2.ForeColor = System.Drawing.Color.Black;
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Name = "label1";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Red;
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Name = "label2";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // textBox3
+            // 
+            resources.ApplyResources(this.textBox3, "textBox3");
+            this.textBox3.Name = "textBox3";
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Name = "label3";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // VK
+            // 
+            resources.ApplyResources(this.VK, "VK");
+            this.VK.BackColor = System.Drawing.Color.Transparent;
+            this.VK.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.VK.Name = "VK";
+            this.VK.Click += new System.EventHandler(this.VK_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Silver;
+            resources.ApplyResources(this.button3, "button3");
+            this.button3.ForeColor = System.Drawing.Color.Black;
+            this.button3.Name = "button3";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.Transparent;
+            resources.ApplyResources(this.linkLabel1, "linkLabel1");
+            this.linkLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabel1.DisabledLinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.LinkColor = System.Drawing.Color.Gray;
+            this.linkLabel1.LinkVisited = true;
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // LAPI
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.VK);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.Restart);
+            this.Controls.Add(this.Start);
+            this.Name = "LAPI";
+            this.Load += new System.EventHandler(this.LAPI_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Button Restart;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label VK;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ src/hittsad/Form1.cs
@@ -0,0 +1,276 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.IO;
+
+
+namespace hittsad
+{
+    public partial class LAPI : Form
+    {
+        public LAPI()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            string path = @"c:\API.txt";
+
+            string APIVolume = textBox3.Text;
+            // Запись ID сервера в txt для памяти.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(APIVolume);
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            string path = @"c:\ServerCode.txt";
+
+            string ClientIDValue = textBox2.Text;
+            // Запись ID сервера в txt для памяти.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(ClientIDValue);
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string pathid = @"c:\ServerCode.txt";
+                string pathapi = @"c:\API.txt";
+                // Чтение ID сервера из txt.
+                using (StreamReader sr = File.OpenText(pathid))
+                {
+                    using (StreamReader sr2 = File.OpenText(pathapi))
+                    {
+                        string ClientIDValue = "";
+                        string APIVolume = "";
+                        ClientIDValue = sr.ReadLine();
+                        APIVolume = sr2.ReadLine();
+                        using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.xeinhost.ru/api/client/servers/" + ClientIDValue + "/power"))
+                        {
+                            request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                            request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + APIVolume);
+
+                            request.Content = new StringContent("{\"signal\": \"start\"}");
+                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                            var response = httpClient.SendAsync(request);
+                            var responseResult = response.Result;
+
+                            if (responseResult.IsSuccessStatusCode)
+                            {
+                                var result = responseResult.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Restart_Click(object sender, EventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string pathid = @"c:\ServerCode.txt";
+                string pathapi = @"c:\API.txt";
+                // Чтение ID сервера из txt.
+                using (StreamReader sr = File.OpenText(pathid))
+                {
+                    using (StreamReader sr2 = File.OpenText(pathapi))
+                    {
+                        string ClientIDValue = "";
+                        string APIVolume = "";
+                        ClientIDValue = sr.ReadLine();
+                        APIVolume = sr2.ReadLine();
+                        using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.xeinhost.ru/api/client/servers/" + ClientIDValue + "/power"))
+                        {
+                            request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                            request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + APIVolume);
+
+                            request.Content = new StringContent("{\"signal\": \"restart\"}");
+                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                            var response = httpClient.SendAsync(request);
+                            var responseResult = response.Result;
+
+                            if (responseResult.IsSuccessStatusCode)
+                            {
+                                var result = responseResult.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string pathid = @"c:\ServerCode.txt";
+                string pathapi = @"c:\API.txt";
+                // Чтение ID сервера из txt.
+                using (StreamReader sr = File.OpenText(pathid))
+                {
+                    using (StreamReader sr2 = File.OpenText(pathapi))
+                    {
+                        string ClientIDValue = "";
+                        string APIVolume = "";
+                        ClientIDValue = sr.ReadLine();
+                        APIVolume = sr2.ReadLine();
+                        using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.xeinhost.ru/api/client/servers/" + ClientIDValue + "/power"))
+                        {
+                            request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                            request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + APIVolume);
+
+                            request.Content = new StringContent("{\"signal\": \"stop\"}");
+                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                            var response = httpClient.SendAsync(request);
+                            var responseResult = response.Result;
+
+                            if (responseResult.IsSuccessStatusCode)
+                            {
+                                var result = responseResult.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string pathid = @"c:\ServerCode.txt";
+                string pathapi = @"c:\API.txt";
+                // Чтение ID сервера из txt.
+                using (StreamReader sr = File.OpenText(pathid))
+                {
+                    using (StreamReader sr2 = File.OpenText(pathapi))
+                    {
+                        string ClientIDValue = "";
+                        string APIVolume = "";
+                        ClientIDValue = sr.ReadLine();
+                        APIVolume = sr2.ReadLine();
+                        using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.xeinhost.ru/api/client/servers/" + ClientIDValue + "/power"))
+                        {
+                            request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                            request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + APIVolume);
+
+                            request.Content = new StringContent("{\"signal\": \"kill\"}");
+                            request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                            var response = httpClient.SendAsync(request);
+                            var responseResult = response.Result;
+
+                            if (responseResult.IsSuccessStatusCode)
+                            {
+                                var result = responseResult.Content.ReadAsStringAsync().Result;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void LAPI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            string path = @"c:\Command.txt";
+
+            string CommandValue = textBox1.Text;
+            // Запись ID сервера в txt для памяти.
+            using (StreamWriter sw = File.CreateText(path))
+            {
+                sw.WriteLine(CommandValue);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                string pathid = @"c:\ServerCode.txt";
+                string pathapi = @"c:\API.txt";
+                string pathcommand = @"c:\Command.txt";
+                // Чтение ID сервера из txt.
+                using (StreamReader sr = File.OpenText(pathid))
+                {
+                    using (StreamReader sr2 = File.OpenText(pathapi))
+                    {
+                        using (StreamReader sr3 = File.OpenText(pathcommand))
+                        {
+                            string ClientIDValue = "";
+                            string APIVolume = "";
+                            string CommandValue = "";
+                            ClientIDValue = sr.ReadLine();
+                            APIVolume = sr2.ReadLine();
+                            CommandValue = sr3.ReadLine();
+                            using (var request = new HttpRequestMessage(new HttpMethod("POST"), "https://panel.xeinhost.ru/api/client/servers/" + ClientIDValue + "/command"))
+                            {
+                                request.Headers.TryAddWithoutValidation("Accept", "application/json");
+                                request.Headers.TryAddWithoutValidation("Authorization", "Bearer " + APIVolume);
+
+                                request.Content = new StringContent("{\"command\": "+CommandValue+"}");
+                                request.Content.Headers.ContentType = MediaTypeHeaderValue.Parse("application/json");
+
+                                var response = httpClient.SendAsync(request);
+                                var responseResult = response.Result;
+
+                                if (responseResult.IsSuccessStatusCode)
+                                {
+                                    var result = responseResult.Content.ReadAsStringAsync().Result;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void VK_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
